d34f20aa6bf9ab2b93cfdede54c2a63e051f411d
chukwuyem11 <chukwuyem99@gmail.com>
2022-08-28 14:43:44 +0100

    first commit

--- components/home.js
+++ components/home.js
@@ -16,46 +16,54 @@ import { Text, Box, Button, ButtonGroup, FormControl,
       {
         id: 1,
         image:"past15",
+        name: "mini-web landing page",
         link: "https://min-website-roan.vercel.app/"
         
       },
       {
         id: 2,
-        image:"past14",
-        link: "https://min-website-roan.vercel.app/dashboard"
+        image:"past16",
+        name: "note taking app",
+        link: "https://nootes.herokuapp.com/"
         
       },
       {
         id: 3,
         image:"past12",
+        name: "chaart landing page",
         link: "https://chaaart.vercel.app/"
         
       },
       {
         id: 4,
         image:"past13",
+        name: "simple landing page",
         link: "https://landing2-henna.vercel.app/"
         
       },
       {
         id: 5,
         image:"mami",
+        name: "social commerce app (mobile)",
         link: "https://mamihq.herokuapp.com"
         
       },
     {
       id: 6,
       image:"crocs",
+      name: "e-commerce crocs app",
       link: "https://github.com/chukwuyem11/crocs-commerce"
       
     },
     {id: 7,
       image:"past7",
+      name: "personal site",
       link: "https://github.com/chukwuyem11/personal_website"
       
     },
     {id: 8,
       image:"past8",
+      name: "simple todo",
       link: "https://github.com/chukwuyem11/simple-todo"
       
     },
@@ -73,25 +81,28 @@ import { Text, Box, Button, ButtonGroup, FormControl,
           <Text color="#ffffff" fontSize={["25px","30px", "35px" ]} fontWeight="900" lineHeight={["25px", "35px", "40px"]} fontFamily="IBM Plex Mono" textAlign={["left","center", "left"]}>Skilled In</Text>
           <Flex mb="50px" mt="50px" >
           
-            <Box mr="20px" fontFamily="IBM Plex Mono">
+          <Box mr="20px" fontFamily="IBM Plex Mono" >
             
-          <UnorderedList>
-  <ListItem fontSize={["16px","18px", "20px" ]} color="#ffffff">React</ListItem>
-
-  <ListItem fontSize={["16px","18px", "20px" ]} color="#ffffff">Javascript</ListItem>
-  <ListItem fontSize={["16px","18px", "20px" ]} color="#ffffff">Emotion js</ListItem>
-  
-</UnorderedList>
-</Box>
-<Box>
-          <UnorderedList>
-          
-
-  <ListItem fontSize={["16px","18px", "20px" ]} color="#ffffff">Next js</ListItem>
-  <ListItem fontSize={["16px","18px", "20px" ]} color="#ffffff">CSS</ListItem>
-  <ListItem fontSize={["16px","18px", "20px" ]} color="#ffffff">Framer Motion</ListItem>
-</UnorderedList>
-</Box>
+            <UnorderedList>
+    <ListItem textAlign={["left","center", "left"]} fontSize={["16px","18px", "20px" ]} color="#ffffff">React</ListItem>
+    <ListItem textAlign={["left","center", "left"]} fontSize={["16px","18px", "20px" ]} color="#ffffff">Node js</ListItem>
+    <ListItem textAlign={["left","center", "left"]} fontSize={["16px","18px", "20px" ]} color="#ffffff">Express js</ListItem>
+    <ListItem textAlign={["left","center", "left"]} fontSize={["16px","18px", "20px" ]}color="#ffffff">Javascript</ListItem>
+    <ListItem textAlign={["left","center", "left"]} fontSize={["16px","18px", "20px" ]} color="#ffffff">Mysql</ListItem>
+    <ListItem textAlign={["left","center", "left"]} fontSize={["16px","18px", "20px" ]}color="#ffffff">Postgresql</ListItem>
+    
+  </UnorderedList>
+  </Box>
+  <Box fontFamily="IBM Plex Mono">
+            <UnorderedList>
+            <ListItem textAlign={["left","center", "left"]} fontSize={["16px","18px", "20px" ]}color="#ffffff">Nest js</ListItem>
+    <ListItem textAlign={["left","center", "left"]} fontSize={["16px","18px", "20px" ]} color="#ffffff">React Native Web</ListItem>
+    <ListItem textAlign={["left","center", "left"]} fontSize={["16px","18px", "20px" ]} color="#ffffff">Next js</ListItem>
+    <ListItem textAlign={["left","center", "left"]} fontSize={["16px","18px", "20px" ]} color="#ffffff">Public Speaking</ListItem>
+    <ListItem textAlign={["left","center", "left"]} fontSize={["16px","18px", "20px" ]}color="#ffffff">Communication skills</ListItem>
+    <ListItem textAlign={["left","center", "left"]} fontSize={["16px","18px", "20px" ]}color="#ffffff">Wordpress</ListItem>
+  </UnorderedList>
+  </Box>
 
 </Flex>
           </Box>
@@ -137,14 +148,17 @@ import { Text, Box, Button, ButtonGroup, FormControl,
   <Text mb="50px" color="#ffffff" fontSize={["25px","30px", "35px" ]} fontWeight="900" lineHeight={["25px", "35px", "40px"]} fontFamily="IBM Plex Mono"  textAlign={["left","center", "left"]}>My Works</Text>
 </Box>
 <Box display={["block", "none", "block"]}>
-<Grid   templateColumns="repeat(2, 1fr)" gap={[6, -6,-6]} mb="50px">
-{Works.map((work) => (<Box key={work.id} mt={["10px","20px","30px"]} ml={["","-11px","-11px"]} ><Link href={work.link}><Image src={`${work.image}.png`}  w={["150px", "200","250px"]} h={["150px", "200","250px"]} borderRadius="5%" bg="#353535"    alt="Segun Adebayo"  p="0" /></Link></Box>))}
+<Grid   templateColumns={["repeat(1, 1fr)","repeat(2, 1fr)","repeat(2, 1fr)"] }gap={[6, -6,-6]} mb="50px">
+{Works.map((work) => (<Box key={work.id} mt={["10px","20px","30px"]} ml={["","-11px","-11px"]} ><Link href={work.link}><Image src={`${work.image}.png`}  w={["80vw", "200","250px"]} h={["auto", "200","250px"]} borderRadius="5%" bg="#353535"    alt="Segun Adebayo"  p="0" /></Link>
+<Text mt="10px" color="#fff" fontSize={["16px","20px", "20px" ]} fontWeight="500"  fontFamily="IBM Plex Mono">{work.name}</Text></Box>))}
 </Grid>
 </Box>
 
 <Center display={["none", "block", "none"]}>
 <Grid  templateColumns="repeat(2, 1fr)" gap={[6, 6,-6]} mb="50px">
-{Works.map((work) => (<Box key={work.id} mt={["10px","20px","30px"]} ml={["","11px","-11px"]} ><Link href={work.link}><Image src={`${work.image}.png`}  w={["150px", "250px","250px"]} h={["150px", "250px","250px"]} borderRadius="5%" bg="#353535"    alt="Segun Adebayo"  p="0" /></Link></Box>))}
+{Works.map((work) => (<Box key={work.id} mt={["10px","20px","30px"]} ml={["","11px","-11px"]} ><Link href={work.link}><Image src={`${work.image}.png`}  w={["150px", "250px","250px"]} h={["150px", "250px","250px"]} borderRadius="5%" bg="#353535"    alt="Segun Adebayo"  p="0" /></Link>
+    <Text color="#fff" fontSize={["25px","30px", "35px" ]} fontWeight="500"  fontFamily="IBM Plex Mono">{work.name}</Text>
+</Box>))}
 </Grid>
 </Center>
 
